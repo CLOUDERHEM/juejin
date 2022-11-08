b21296032d94d5603f56987fb6b5b7d4cc544377
郑叹 <895898088@qq.com>
2022-11-08 15:19:50 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 axios.post('https://api.juejin.cn/growth_api/v1/check_in', {
   headers: {
-    "content-type": "application/json"
+    "content-type": "application/json",
     "cookie": process.env.COOKIE,
   }
 }).then((res) => console.log(res));
